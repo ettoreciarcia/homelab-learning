cb66f4376dac23757414e22e23220eb1fc3e4956
ettoreciarcia <ettore.ciarcia@epsilonline.com>
2022-03-08 17:09:51 +0100

    Add skeleton Random Meme Generator

---
+++ RandomGIF-Get/app.js
@@ -0,0 +1,73 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/random', (req, res) => {
+  res.send('You are in random path and you are using GET method')
+})
+
+app.post('/random', (req, res) => {
+  res.send('You are in random path and you are using POST method')
+})
+
+app.put('/random', (req, res) => {
+  res.send('You are in random path and you are using PUT method')
+})
+
+app.delete('/random', (req, res) => {
+  res.send('You are in random path and you are using delete method')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//Serve a file in public directory
+app.use(express.static('public'))
