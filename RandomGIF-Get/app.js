9b7d857f261491a7dd6e562929f421e9636f97a8
ettoreciarcia <ettore.ciarcia@epsilonline.com>
2022-03-08 18:25:11 +0100

    Added boris meme and logging

--- RandomGIF-Get/app.js
+++ RandomGIF-Get/app.js
@@ -1,49 +1,15 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
+var logger = require('morgan')
+
+//logging
+app.use(logger('combined', {
+  stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
+app.use(logger('dev'));
+//end logging
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -71,3 +37,11 @@ app.listen(port, () => {
 
 //Serve a file in public directory
 app.use(express.static('public'))
+
+
+
+app.get('/index', (req,res) =>{
+  let index = Math.floor(Math.random() * 11);
+  res.send('Your index value is equal to' + index)
+  console.log('Value generate for user is equal to ' + index)
+})
