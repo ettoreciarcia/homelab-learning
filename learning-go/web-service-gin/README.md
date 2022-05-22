7cbfbcdfbde34309a914bd7036c3172dd6b90133
ettoreciarcia <ettore.ciarcia@epsilonline.com>
2022-05-22 12:22:11 +0200

    Added README

---
+++ learning-go/web-service-gin/README.md
@@ -0,0 +1,19 @@
+# Kubetesting 
+
+A simple application to test some services within Kubernetes.
+It aims to test passage of environment variables, reads and writes on different types of databases, reads and writes on services similar to s3 and generates traffic spikes when a certain function is invoked
+
+## Roadmap
+
+- [x] Hello World
+- [x] Get a specific env
+- [x] Get all envs
+- [x] Health Check
+- [x] Query String
+- [ ] Return HTTP request
+- [ ] Postgres
+- [ ] MySQL
+- [ ] Scylla
+- [ ] S3
+- [ ] Basic Auth
+- [ ] Peak generator
--- learning-go/web-service-gin/main.go
+++ learning-go/web-service-gin/main.go
@@ -23,6 +23,21 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+type path struct {
+	Path        string `json:"path"`
+	Description string `json:"description"`
+}
+
+var paths = []path{
+	{Path: "/helloWorld", Description: "A sample Hello World response"},
+	{Path: "/getAlbums", Description: "Get all albums in a json"},
+	{Path: "/getEnv", Description: "Get a specified env"},
+	{Path: "/getEnvs", Description: "Get all env"},
+	{Path: "/healtCheck", Description: "Health Check"},
+	{Path: "/helloPerson", Description: "Testing query string"},
+	{Path: "/", Description: "List all available path"},
+}
+
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
@@ -49,6 +64,15 @@ func healthCheck(c *gin.Context) {
 	c.String(http.StatusOK, "Health Check returns status 200")
 }
 
+func helloPerson(c *gin.Context) {
+	firstname := c.DefaultQuery("firstname", "Guest")
+	c.String(http.StatusOK, "Hello %s", firstname)
+}
+
+func home(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, paths)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
@@ -56,7 +80,8 @@ func main() {
 	router.GET("/env", getEnv)
 	router.GET("/envs", getEnvs)
 	router.GET("healthCheck", healthCheck)
-
-	router.Run("localhost:8080")
+	router.GET("/", home)
+	router.POST("/helloPerson", helloPerson)
+	router.Run("0.0.0.0:8080")
 
 }
